9d84eb51825a9c1aa1c59bdbc5a9629463740bad
leanth <leanth@oregonstate.edu>
2018-02-13 07:24:08 -0800

    Change ode function filename

--- main.m
+++ main.m
@@ -2,22 +2,23 @@
 close all;
 clear;
 
+global tau_c tau_1 tau_2 A;
 parameters;
 
 t_span = [0 800];
 int = [C_N0; F0];
 
-[t, dydt] = ode45(@musclef_ode, t_span, int);
+[t, dY] = ode45(@muscleforce_ode, t_span, int);
 
 figure(1);
 subplot(2, 1, 1);
-plot(t, dydt(:, 1));
+plot(t, dY(:, 1));
 xlabel('Time (ms)');
 ylabel('C_{N}');
-ylim([0 0.2+max(dydt(:, 1))]);
+ylim([0 0.2+max(dY(:, 1))]);
 subplot(2, 1, 2);
-plot(t, dydt(:, 2));
+plot(t, dY(:, 2));
 xlabel('Time (ms)');
 ylabel('Force (N)');
-ylim([0 10+max(dydt(:, 2))]);
+ylim([0 10+max(dY(:, 2))]);
 
